39a438c8ef2b5ee01173103f1ea718204cce5f5a
LoganArnett <Loganarn5@gmail.com>
2015-04-23 16:16:50 -0400

    added dynamic titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,22 +7,38 @@ describe "Static pages" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+      
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
     end 
   end
     
-  describe "Help Page" do 
+  describe "Help page" do
       
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
-    end
-  end 
+   end
+      
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end 
+  end
     
-    describe "About Page" do
-        
-      it "should have the content 'About Us'" do
-        visit '/static_pages/about'
-        expect(page).to have_content('About Us')
+  describe "About page" do
+      
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
+      
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end 
   end
 end
+
